f7a1ccd7c9029fb51b6eb802c6bd9e287f5dbefc
Sarang Hadagali <sarang.hadagali@gmail.com>
2022-04-05 12:29:14 -0400

    test

--- public/index.js
+++ public/index.js
@@ -3,19 +3,19 @@
 document.getElementById("submit").onclick = async () => {
     //const buttonSelected = document.querySelector('input[name="userChoice"]:checked').value
     const customWord = document.getElementById('customWord').checked
-    if(!customWord){
+    if (!customWord) {
         const json = await (await fetch(`http://localhost:3000/check/?word=${document.getElementById("inputWord").value}`)).json()
         if (json == 'Entry word not found') {
             alert("Word does not exist")
             return
         }
     }
-    const response = await (await fetch('/generateURL', { 
+    const response = await (await fetch('/generateURL', {
         method: 'post',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({word: document.getElementById("inputWord").value, customWord})
+        body: JSON.stringify({ word: document.getElementById("inputWord").value, customWord })
     })).json()
     console.log(response, response.path)
     const url = `${location.href}paths/${response.path}?customWord=${response.customWord}`
